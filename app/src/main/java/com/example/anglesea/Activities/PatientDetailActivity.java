a2e71498cde2439b739d1ad9c37895ab5cb50f64
Kizari <kizari93@gmail.com>
2018-06-20 11:27:58 +1200

    Merge

--- app/src/main/java/com/example/anglesea/Activities/PatientDetailActivity.java
+++ app/src/main/java/com/example/anglesea/Activities/PatientDetailActivity.java
@@ -3,10 +3,14 @@ package com.example.anglesea.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PatientDetailActivity extends AppCompatActivity {
+import com.example.anglesea.R;
+
+public class PatientDetailActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_detail);
     }
